ab0cdb98a52ba4712994715b0302d042dee860db
Miguel Chaves <miguel@gdchaves.com>
2022-01-12 18:33:48 +0000

    Corregir selector y orden de campos al crear estudiante

--- create-student.php
+++ create-student.php
@@ -8,16 +8,16 @@ $bdConnection = $conexionBD->getConexionBD();
 // // Variable global para restringir el acceso directo a otros ficheros
 define('_APPINIT', 1);
 
-
-
-$nuevo = array( $_POST["inputName"],
-                $_POST["inputApellido"],
-                $_POST["inputEmail"],
-                $_POST["inputTipoId"],
-                $_POST["inputNumId"],
-                $_POST["inputPrefijoTel"],
-                $_POST["inputNumTel"]);//Array con los datos del nuevo usuario    
-                echo $nuevo[5];
+$nuevo = array(
+  $_POST["inputName"],
+  $_POST["inputApellido"],
+  $_POST["inputEmail"],
+  $_POST["inputPrefijoTel"],
+  $_POST["inputNumTel"],
+  $_POST["inputTipoId"],
+  $_POST["inputNumId"]
+); //Array con los datos del nuevo usuario    
+echo $nuevo[5];
 require './src/controllers/ListaEstudiantes.php';
 new AgregarEstudiantes($bdConnection,$nuevo[0], $nuevo[1], $nuevo[2], $nuevo[3], $nuevo[4], $nuevo[5], $nuevo[6]);
 
--- src/views/addEstudiante.php
+++ src/views/addEstudiante.php
@@ -38,15 +38,15 @@
             </div>
             <div class="mb-1">
                 <label for="inputTipoId" class="form-label">Tipo Identificación</label>
-                <input type="text" class="form-control" id="inputTipoId" name="inputTipoId">
-            </div>
-            <div class="mb-1">
-                <label for="inputNumId" class="form-label">N° Identificación</label>
                 <select class="form-control form-select" aria-label="Default select example" id="inputTipoId" name="inputTipoId">
                     <option value="DNI">DNI - Documento Nacional de Identidad</option>
                     <option value="NIE">NIE - Número de Identificación de Extranjeros</option>
                     <option value="PASSPORT">Pasaporte</option>
                 </select>
+            </div>
+            <div class="mb-1">
+                <label for="inputNumId" class="form-label">N° Identificación</label>
+                <input type="text" class="form-control" id="inputNumId" name="inputNumId">
             </div>
 
             <div class="mb-1">
